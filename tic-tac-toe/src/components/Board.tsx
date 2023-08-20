f7e8f739d9694e2f0457f0ddf64fb77d428d2faf
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-08-20 02:02:37 +0200

    added confetti_animation

--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -1,32 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Square from './Square';
 import { checkWinner } from '../utils/checkForWinner';
+import { useGameContext } from './GameContext';
+import ConfettiAnimation from './ConfettiAnimation';
+import '../App.css';
 
-type Props = {
-  changePlayer: () => void;
-  currentPlayer: string;
-};
+const Board: React.FC = () => {
+  const { currentPlayer, changePlayer } = useGameContext(); // Accessing currentPlayer from context
 
-const Board: React.FC<Props> = ({ changePlayer, currentPlayer }) => {
   const [squares, setSquares] = useState(Array(9).fill(''));
   const [winningPlayer, setWinningPlayer] = useState('');
+  const [isDraw, setIsDraw] = useState(false);
 
   const handleClick = (cellIdx: number) => {
     const grid = [...squares];
     grid[cellIdx] = currentPlayer;
-
     setSquares(grid);
+    changePlayer();
+  };
+
+  const restartGame = () => {
+    window.location.reload();
   };
 
   useEffect(() => {
     const winner = checkWinner(squares);
     if (winner) {
       setWinningPlayer(winner);
+    } else if (squares.every((square) => square !== '')) {
+      setIsDraw(true);
     }
   }, [squares]);
 
+  const renderDrawModal = () => {
+    if (isDraw) {
+      return (
+        <div className="modal">
+          <h2>It's a draw!</h2>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className='board'>
+    <div className="board">
       {squares.map((_, index) => (
         <Square
           key={index}
@@ -37,6 +54,11 @@ const Board: React.FC<Props> = ({ changePlayer, currentPlayer }) => {
         />
       ))}
       {winningPlayer && <h2>{winningPlayer} wins!</h2>}
+      {winningPlayer && <ConfettiAnimation />}
+      {renderDrawModal()}
+      <button className="restart-button" onClick={restartGame}>
+        Restart
+      </button>
     </div>
   );
 };
